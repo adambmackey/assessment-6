4a18f6ac3569a8a50b5d2515fe4b62ff7e954ef8
Adam Mackey <adambmackey@gmail.com>
2022-04-08 15:55:39 -0400

    updated commit

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,22 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    describe('suffleArray should', () => {
+        test('shuffleArray should return an array', () => {
+            let arr =  [1,2,3,4,5]
+            let result = shuffleArray(arr)
+            expect(result).toHaveProperty('length')
+        })
+    })
+    test('shuffleArray should return an array of the same length', () => {
+        let arr =  [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(result.length).toEqual(arr.length)
+    })
+
+    test('shuffleArray should return an array with the same item', () => {
+        let arr =  [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(result).toEqual(expect.arrayContaining(arr))
+    })
 })
